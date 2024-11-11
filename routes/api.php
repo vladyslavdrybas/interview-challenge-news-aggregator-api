712ce145e0b872183a931ce25de671d29d7f6df9
Vladyslav Drybas <8395461+vladyslavdrybas@users.noreply.github.com>
2024-11-11 21:48:43 +0100

    add middleware auth for manual test

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\ApiHomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/v'. config('app.api.version'))->group(function () {
-    Route::get('/', [ApiHomeController::class, 'index'])->name('api.home');
-});
+Route::prefix('/v'. config('app.api.version'))->middleware(['auth:sanctum'])
+    ->group(function () {
+        Route::get('/', [ApiHomeController::class, 'index'])->name('api.home');
+    });
