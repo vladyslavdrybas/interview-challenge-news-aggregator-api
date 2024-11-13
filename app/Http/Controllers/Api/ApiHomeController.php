8a9841bbdfc93803dca3ff1582ed50a04f9e9c6e
Vladyslav Drybas <8395461+vladyslavdrybas@users.noreply.github.com>
2024-11-13 14:47:34 +0100

    remove redundant code

--- app/Http/Controllers/Api/ApiHomeController.php
+++ app/Http/Controllers/Api/ApiHomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use OpenApi\Attributes as OA;
 
 class ApiHomeController extends Controller
@@ -35,37 +34,5 @@ class ApiHomeController extends Controller
         return response()->json([
             'message' => 'Welcome to the API!'
         ]);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
