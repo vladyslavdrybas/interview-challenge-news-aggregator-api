f26b0bf032546da19ba2856abe3679eb2d4f0738
Vladyslav Drybas <8395461+vladyslavdrybas@users.noreply.github.com>
2024-11-13 15:16:30 +0100

    remove redundant code

--- database/factories/NewsSourceFactory.php
+++ database/factories/NewsSourceFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\ModelFactories\NewsSourceModelFactory;
 use App\Models\NewsSource;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
