9960f6dcb240e99f88c2d492b599db8191e99288
Vladyslav Drybas <8395461+vladyslavdrybas@users.noreply.github.com>
2024-11-14 15:12:40 +0100

    finish guardian basic grabber.

--- app/Services/NewsGrabber/Business/Grabbers/Strategies/GuardianGrabber.php
+++ app/Services/NewsGrabber/Business/Grabbers/Strategies/GuardianGrabber.php
@@ -8,28 +8,65 @@ use App\Services\NewsGrabber\Business\Grabbers\IGrabber;
 use App\Services\NewsGrabber\DTO\FetchedArticleDTO;
 use App\Services\NewsGrabber\DTO\FetchedNewsDTO;
 use App\Services\NewsGrabber\DTO\NewsSourceDTO;
-use DateTime;
+use Carbon\Carbon;
 use Exception;
-use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use function collect;
+use function sprintf;
 
 class GuardianGrabber implements IGrabber
 {
+    protected ?string $apiUrl = null;
+    protected ?string $apiKey = null;
+
+    public function __construct()
+    {
+        $this->apiKey = env('GUARDIAN_API_KEY');
+        $this->apiUrl = env('GUARDIAN_API_BASE_URL');
+    }
+
+    // TODO add limits to reduce requests for each api based on the "api activity"
+    // TODO get categories, authors, etc.
     public function fetch(NewsSourceDTO $source): FetchedNewsDTO
     {
         if (StrategyNames::tryFrom($source->strategy) !== StrategyNames::GUARDIAN) {
             throw new Exception(sprintf('%s is a wrong strategy for source: %s', StrategyNames::GUARDIAN->value, $source->strategy));
         }
 
-        // TODO add validation before writing
-        $news = [
-            new FetchedArticleDTO(
-                title: StrategyNames::GUARDIAN->value . '-' . Str::random() . uniqid('', true),
-                sourceSlug: $source->slug,
-                publishedAt: new DateTime()
-            ),
-        ];
+        $url = $this->apiUrl ?? $source->url ?? null;
+        $apikey =  $this->apiKey ?? $source->apikey ?? null;
 
-        throw new Exception('TODO ' . __METHOD__);
+        if (empty($apikey) || empty($url)) {
+                Log::warning(sprintf('%s cannot take data via %s. Empty input values.', StrategyNames::GUARDIAN->value, $source->strategy));
+                return new FetchedNewsDTO([]);
+        }
+
+        $response = Http::get(
+            $url,
+            [
+                'api-key' => $apikey,
+                'page-size' => 50,
+                'page' => 1,
+                'order-by' => 'newest',
+            ]
+        );
+
+        if (!$response->successful()) {
+            Log::warning(sprintf('%s cannot take data via %s', StrategyNames::GUARDIAN->value, $source->strategy));
+            return new FetchedNewsDTO([]);
+        }
+
+        $articles = $response->json('response.results');
+
+        $news = collect($articles)->map(function ($article) use ($source) {
+            return new FetchedArticleDTO(
+                title: $article['webTitle'],
+                content: $article['webUrl'],
+                sourceSlug: $source->slug,
+                publishedAt: Carbon::parse($article['webPublicationDate'])
+            );
+        })->toArray();
 
         return new FetchedNewsDTO($news);
     }
